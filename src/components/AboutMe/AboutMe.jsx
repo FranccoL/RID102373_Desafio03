e94487ed4527243ea58836792cb13da72ad32396
FranccoL <leofranco96@gmail.com>
2025-01-17 11:24:47 -0300

    "feat: adicionar funcionalidade de exibição do perfil na seção About Me"

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -16,7 +16,10 @@ function AboutMe() {
 
   return (
     <div className="container" >
+        
         <div className="tittleAbout d-flex flex-column" >
+        <div className="imgProfile d-flex"></div>
+        <div className="text d-flex flex-column">
         <h1>Olá, me chamo Leonardo</h1>
         <p>
         Tenho 28 anos e estou cursando o curso de Full Stack pela escola DNC. Desde muito jovem, sempre fui fascinado pelo mundo da tecnologia. Ao longo dos anos, essa paixão cresceu e se transformou em uma carreira.
@@ -24,12 +27,14 @@ function AboutMe() {
           {showMore && (
             <>
               ao longo do tempo, fui aperfeiçoando minhas habilidades, inicialmente com HTML e CSS. Com o tempo, fui me aprofundando mais em linguagens como JavaScript e, mais recentemente, explorando o ecossistema de desenvolvimento web com React, Node.js e bancos de dados.
-
-Ao longo dessa jornada, percebi que o que mais me atrai no desenvolvimento é a possibilidade de criar soluções que podem impactar diretamente a vida das pessoas. Seja criando sites, aplicativos ou ferramentas, a ideia de transformar ideias em realidade através de código é o que me motiva diariamente.
+              Ao longo dessa jornada, percebi que o que mais me atrai no desenvolvimento é a possibilidade de criar soluções que podem impactar diretamente a vida das pessoas. Seja criando sites, aplicativos ou ferramentas, a ideia de transformar ideias em realidade através de código é o que me motiva diariamente.
             </>
           )}
         </p>
-            <button className='btAboutMe' onClick={toggleShowMore}> {showMore ? 'Mostrar menos' : 'Saber mais'}</button>
+        <button className='btAboutMe ' onClick={toggleShowMore}> {showMore ? 'Mostrar menos' : 'Saber mais'}</button>
+        </div>
+        
+            
         </div>
     </div>
   )
